c28900f01cfb295f485bdf8f3dbc575b4119b90b
MartinGaiser <martin-gaiser@t-online.de>
2019-03-19 13:33:32 +0100

    fixed missing blanks in user_test

--- moviestore/user_test.go
+++ moviestore/user_test.go
@@ -8,8 +8,8 @@ func TestUser(t *testing.T) {
 		want string
 	}{
 		{User{"Heinz Ketchup", 12, 12}, "  12. Heinz Ketchup, 12"},
-		{User{"Hendlmaier Senf", 13, 8}, "   8. Hendlmaier Senf,13"},
-		{User{"Löwensenf", 14, 13}, "  13. Löwensenf,14"},
+		{User{"Hendlmaier Senf", 13, 8}, "   8. Hendlmaier Senf, 13"},
+		{User{"Löwensenf", 14, 13}, "  13. Löwensenf, 14"},
 	}
 	for _, c := range cases {
 		got := c.in.String()
