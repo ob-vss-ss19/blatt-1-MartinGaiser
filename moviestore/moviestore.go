d4cb789673ea5bb449b896753280b89fbfaab465
Martin Gaiser <martin-gaiser@t-online.de>
2019-03-20 11:19:58 +0100

    implemented return and rentByUser methods

--- moviestore/moviestore.go
+++ moviestore/moviestore.go
@@ -1,5 +1,7 @@
 package moviestore
 
+import "errors"
+
 type moviestoreImpl struct {
 	available  map[Serial]Movie
 	rented     map[UserID][]Movie
@@ -19,25 +21,56 @@ func (ms *moviestoreImpl) AddUser(name string, age Age) UserID {
 	nextUserID := ms.nextUserID
 	ms.nextUserID++
 	ms.users[nextUserID] = User{name,age,nextUserID}
+	ms.rented[nextUserID] = []Movie{}
 	return nextUserID
 }
 
 func (ms *moviestoreImpl) Rent(serial Serial, userID UserID) (User, Movie, error) {
 	movie, isAvailable := ms.available[serial]
-	_, userExists := ms.users[userID]
+	user, userExists := ms.users[userID]
 	if isAvailable && userExists{
 		delete(ms.available,serial)
 		ms.rented[userID] = append(ms.rented[userID],movie)
+		return user, movie,nil
+	}else if isAvailable && !userExists{
+		return User{},Movie{},errors.New("no User with the ID: " + string(userID) + " was found")
+	}else if !isAvailable && userExists{
+		return User{},Movie{},errors.New("the Movie with the Serial: " + string(serial) + " was not found. It might have been rented")
 	}else{
-		//error
+		return User{},Movie{},errors.New("neither the user nor the movie was found")
 	}
-	return User{}, Movie{}, nil
 }
 
 func (ms *moviestoreImpl) RentedByUser(userID UserID) ([]Movie, error) {
-	return []Movie{{}, {}}, nil
+	rentedMovies,userExists := ms.rented[userID]
+	if userExists{
+		return rentedMovies,nil
+	}else{
+		return []Movie{},errors.New("the User with the UserID: " + string(userID) + " does not exist")
+	}
 }
 
 func (ms *moviestoreImpl) Return(serial Serial) (User, Movie, error) {
-	return User{}, Movie{}, nil
+	_,movieAvailable := ms.available[serial]
+	if movieAvailable{
+		return User{}, Movie{}, errors.New("the Movie can't be returned since it's already available")
+	}
+	var returnMovie Movie
+	var renterUserID UserID
+	for user,rentedMovies := range ms.rented{
+		newRentedMovies := rentedMovies[:0]
+		for _,movie := range rentedMovies{
+			if movie.Serial != serial{
+				newRentedMovies = append(newRentedMovies, movie)
+			}else{
+				returnMovie = movie
+				renterUserID = user
+			}
+		}
+		ms.rented[user] = newRentedMovies
+	}
+	if returnMovie.Serial != serial{
+		return User{}, Movie{}, errors.New("the Movie can't be returned since it it not rented")
+	}
+	return ms.users[renterUserID],returnMovie,nil
 }
